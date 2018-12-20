617be05a2b2c8a9d2a98831dd6ddc1f4f2618ca5
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2018-12-20 13:24:39 -0500

    Add preloaders for celebrities

--- src/components/Celebrities/Celebrities.jsx
+++ src/components/Celebrities/Celebrities.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import ContentLoader from 'react-content-loader'
 import CelebrityBase from './../Celebrity';
 import { withFirebase } from '../Firebase';
 
+const MyLoader = props => (
+	<ContentLoader 
+		height={475}
+		speed={2}
+		primaryColor="#f3f3f3"
+		secondaryColor="#ecebeb"
+		{...props}
+	>
+		<circle cx="155.12" cy="136.18" r="96.57" /> 
+		<rect x="181.55" y="419.61" rx="0" ry="0" width="113.12" height="48" /> 
+		<rect x="7.55" y="420.61" rx="0" ry="0" width="164.51" height="47" /> 
+		<rect x="49.55" y="361.61" rx="0" ry="0" width="25.35" height="23.6" /> 
+		<rect x="79.55" y="361.61" rx="0" ry="0" width="26.4" height="23.6" /> 
+		<rect x="115.55" y="360.61" rx="0" ry="0" width="82.65" height="24.42" /> 
+		<rect x="49.55" y="333.61" rx="0" ry="0" width="209.88" height="13" /> 
+		<rect x="48.55" y="302.61" rx="0" ry="0" width="210.38" height="18" /> 
+		<rect x="7.55" y="299.61" rx="0" ry="0" width="30" height="21.08" />
+	</ContentLoader>
+);
+const loaders = new Array(5).fill(0);
+
 const Celebrities = ({ celebrities, update }) =>  (
   <div className="celebrities">
-    <h1 className="celebrities__title">Previous Rulings</h1>
+    <h1 className="celebrities__title">Votes</h1>
     <div className="celebrities__container">
       {
+        celebrities.length ? 
         celebrities.map(celebrity => {
           const { id, img, name, description, date, category, votes } = celebrity;
           return (
@@ -24,6 +47,10 @@ const Celebrities = ({ celebrities, update }) =>  (
             />
           )
         })
+        : 
+        loaders.map(d => (
+          <MyLoader />
+        ))
       }
     </div>
     <div className="celebrities__background">
