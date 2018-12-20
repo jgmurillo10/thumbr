203dfff0cb90c8a718b502d1a41c478a842891b5
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2018-12-20 18:30:59 -0500

    Updates

---
+++ precache-manifest.92d09d59a8041ad94f60405552c3ea59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e6edd691592161ce175",
+    "url": "/thumbr/static/css/main.b1bd64ab.chunk.css"
+  },
+  {
+    "revision": "7e6edd691592161ce175",
+    "url": "/thumbr/static/js/main.7e6edd69.chunk.js"
+  },
+  {
+    "revision": "d5c9aa4a54ee797964b2",
+    "url": "/thumbr/static/js/1.d5c9aa4a.chunk.js"
+  },
+  {
+    "revision": "8466b800d6ecfb394570",
+    "url": "/thumbr/static/js/runtime~main.8466b800.js"
+  },
+  {
+    "revision": "d3894308e30ad729799c0221e5a28d5c",
+    "url": "/thumbr/static/media/search.d3894308.svg"
+  },
+  {
+    "revision": "fe9488fe09c1cae6e442a271c075771d",
+    "url": "/thumbr/index.html"
+  }
+];
